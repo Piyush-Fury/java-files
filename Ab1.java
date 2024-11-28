aece05abe8265ce834b5f4b4b7e31377ea4a7d60
Piyush Agrawal <107843526+Piyush-Fury@users.noreply.github.com>
2024-11-28 17:20:36 +0530

    SH-256-fury

--- Ab1.java
+++ Ab1.java
@@ -70,7 +70,7 @@ class SmartTelephone extends Telephone{
     // }
 }
 
-//this is the change and now i have changed the pr name
+//this is the change and now i have changed the pr name HOLA AMIGO
 public class Ab1 {
     public static void main(String[] args) {
         Telephone t1 = new SmartTelephone();
