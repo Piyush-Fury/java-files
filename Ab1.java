d2d08b7450932aeba1b1eed431e2f87b1aab9963
Piyush Agrawal <107843526+Piyush-Fury@users.noreply.github.com>
2024-11-28 17:45:39 +0530

    SH-121-update

--- Ab1.java
+++ Ab1.java
@@ -70,7 +70,7 @@ class SmartTelephone extends Telephone{
     // }
 }
 
- 
+// ceck it 
 public class Ab1 {
     public static void main(String[] args) {
         Telephone t1 = new SmartTelephone();
